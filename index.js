adf1feb86ffef5f0d034e8d1946530dc94c94139
Preston-Ak557 <1akmeemanpre@hdsb.ca>
2024-09-12 17:54:01 -0400

    Deleted functions

--- index.js
+++ index.js
@@ -2,41 +2,14 @@
 let x = prompt("What is x?");
 let y = prompt("What is y?");
 
-x = parseFloat(x);
-y = parseFloat(y);
-
-sum = x + y;
-difference = x - y;
-product = x * y;
-quotient = x / y;
+parseFloat(x);
+parseFloat(y);
 
 function addNumbers(num1, num2) {
     const result = num1 + num2;
     return result;
 }
 
-const numberAdd = addNumbers(num1, num2);
+const numberAdd = addNumbers(x, y);
 console.log(numberAdd);
 
-function subtractNumbers() {
-    const result = num1 - num2;
-    return result;
-}
-
-const numberSubtract = subtractNumbers(num1m, num2);
-console.log(numberSubtract);
-
-function divideNumbers() {
-    const result = num1 / num2;
-    return result;
-}
-
-const numberDivide = divideNumbers(num1, num2);
-
-function multiplyNumbers() {
-    const result = num1 * num2
-    return result;
-}
-
-const numberMultiply = multiplyNumbers(num1, num2);
-console.log(numberMultiply);

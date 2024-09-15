12b0b8c43dd9ca11efe8cdb12ac34d18686bd372
Preston-Ak557 <1akmeemanpre@hdsb.ca>
2024-09-15 14:34:42 -0400

    It works but why

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+operation = prompt("What operation do you want to use? (+, -, /, or *)")
 
 let x = prompt("What is x?");
 let y = prompt("What is y?");
@@ -6,15 +7,30 @@ let y = prompt("What is y?");
 x = parseFloat(x);
 y = parseFloat(y);
 
-function addNumbers(num1, num2) {
-    const result = num1 + num2;
-    return result;
+let result;
+
+if (operation == "+") {
+    result = x + y;
+} else if (operation == "-") {
+    result = x - y;
+} else if (operation == "*") {
+    result = x * y;
+} else if (operation == "/") {
+    result = x / y;
+} else if (operation == "+") {
+    result = x + y;
 }
 
-const numberAdd = addNumbers(x, y);
+alert(result);
 
 if (!Number.isInteger(Number(x, y))) {
     alert("Please enter a valid integer");
 }   else {
     alert(numberAdd);
 }
+
+/* Setting a number for x and a letter for y results in NaN but is different vice versa. 
+
+If the program reads the first number as an integer, the proper error message appears, but
+if x is an integer while y is not, NaN appears*/
+

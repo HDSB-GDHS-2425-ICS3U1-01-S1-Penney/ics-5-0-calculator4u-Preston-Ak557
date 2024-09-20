b3391f21ee5857b7ea290a508fdf856f24c1b804
Preston-Ak557 <1akmeemanpre@hdsb.ca>
2024-09-20 18:35:12 -0400

    Fixed NaN error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  // DATE: September 16th, 2024
  // TITLE: Calculator for Two Numbers
 
-operation = prompt("What operation do you want to use? (+, -, /, or *)")
+operation = prompt("What operation do you want to use? (+, -, /, *, sqrt, or ^)")
 
 let x = prompt("What is x?");
 let y = prompt("What is y?");
@@ -28,14 +28,16 @@ if (operation == "+") {
     result = x * y;
 } else if (operation == "/") {
     result = x / y;
+} else if (operation == "sqrt") {
+    result = Math.sqrt(x, y);
 }
 
-console.log(result);
+if (Number.isInteger(result)) {
+    alert(result);
+} else console.log(result);
 
 
 
-/* Setting a number for x and a letter for y results in NaN but is different vice versa. 
 
-If the program reads the first number as an integer, the proper error message appears, but
-if x is an integer while y is not, NaN appears*/
+
 
